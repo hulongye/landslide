f2ddb0cb05a4fb8c00ec277098270bb06d5e861b
Longye Hu <hulongye159@163.com>
2024-06-09 00:34:23 +0800

    初始化

---
+++ cnn.py
@@ -0,0 +1,27 @@
+from keras.models import Sequential
+from keras.layers import Conv1D, MaxPooling1D, Flatten, Dense, Dropout
+from keras import regularizers
+
+
+def cnn_model():
+    model = Sequential()
+    conv1 = Conv1D(filters=15, kernel_size=3, padding='same', activation='tanh', input_shape=(13, 1),
+                   kernel_regularizer=regularizers.L1(0.01))
+    pool1 = MaxPooling1D(strides=1)
+    conv2 = Conv1D(filters=30, kernel_size=3, padding='same', activation='tanh')
+    pool2 = MaxPooling1D(strides=1)
+    conv3 = Conv1D(filters=60, kernel_size=3, padding='same', activation='tanh')
+    pool3 = MaxPooling1D(strides=1)
+    model.add(conv1)
+    model.add(pool1)
+    model.add(conv2)
+    model.add(pool2)
+    model.add(conv3)
+    model.add(pool3)
+    model.add(Flatten())
+    model.add(Dense(60, activation='tanh', kernel_regularizer=regularizers.L1(0.01)))
+    model.add(Dropout(0.2))
+    model.add(Dense(1, activation='sigmoid'))
+
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
---
+++ rf.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+
+
+data = pd.read_excel('../data/test.xlsx')
+data = data.iloc[:, :].astype('float').values
+
+X1 = data[:,0:14]
+X2 = data[:, 14:16]
+y = data[:,16:]
+
+min_max_scaler = MinMaxScaler()
+enc = OneHotEncoder()
+X1 = min_max_scaler.fit_transform(X1)
+X2 = enc.fit_transform(X2).toarray()
+X = np.concatenate((X1, X2), axis=1)
+y = np.array(y).flatten()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+rf = RandomForestClassifier(n_estimators=100)
+rf.fit(X_train, y_train)
+print(rf.feature_importances_)
+print(rf.score(X_test, y_test))
